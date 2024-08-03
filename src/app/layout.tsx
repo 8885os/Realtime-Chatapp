5e73407020a1db8e180d38bbcf100fcc338a6774
Danny <d729258@gmail.com>
2024-08-03 18:06:34 +0100

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 
 // Done after the video and optional: add page metadata
 export const metadata = {
-  title: 'FriendZone | Home',
-  description: 'Welcome to the FriendZone',
+  title: 'Realtime Chatapp | Home',
+  description: 'Welcome to the Chatapp',
 }
 
 export default function RootLayout({
